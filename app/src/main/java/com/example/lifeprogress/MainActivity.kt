02dc3da9a432e983bef3636bc4d2ad6436947ae3
yupenglei <yupengniaosanshi@gmail.com>
2019-11-06 17:15:12 +0800

    添加周期任务

--- app/src/main/java/com/example/lifeprogress/MainActivity.kt
+++ app/src/main/java/com/example/lifeprogress/MainActivity.kt
@@ -1,41 +1,60 @@
 package com.example.lifeprogress
 
 import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.support.v4.app.NotificationCompat
-import android.support.v4.app.NotificationManagerCompat
-import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        btn_notify.setOnClickListener { createNotification() }
+//        btn_notify.setOnClickListener { createNotification() }
+        scheduleJob()
     }
 
-    private fun createNotification() {
-        val intent = Intent(this, ResultActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+    private fun scheduleJob() {
+        val request = PeriodicWorkRequest.Builder(FunnyWork::class.java, 1, TimeUnit.MINUTES).build()
+        WorkManager.getInstance(this).enqueue(request)
+    }
+
+    class FunnyWork(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
+        override fun doWork(): Result {
+            createNotification(applicationContext)
+            return Result.success()
         }
-        val pendingIntent = PendingIntent.getActivity(
-            this, 1,
-            intent, PendingIntent.FLAG_NO_CREATE
-        )
 
-        val builder = NotificationCompat.Builder(this, App.CHANNEL_ID)
-            .setSmallIcon(android.R.drawable.ic_notification_overlay)
-            .setContentTitle("Hello")
-            .setContentText("world")
-            .setContentIntent(pendingIntent)
-            .setAutoCancel(true)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        private fun createNotification(context: Context) {
+            // 通知跳转
+            val intent = Intent(context, ResultActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+            val pendingIntent = PendingIntent.getActivity(context, 1, intent, PendingIntent.FLAG_NO_CREATE)
 
-        with(NotificationManagerCompat.from(this)) {
-            notify(NOTIFICATION_ID, builder.build())
+            // 创建通知
+            val builder = NotificationCompat.Builder(context, App.CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+                .setLargeIcon(BitmapFactory.decodeResource(context.resources, R.mipmap.funny))
+                .setContentTitle("Current Time")
+                .setContentText(Date().toString())
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+            with(NotificationManagerCompat.from(context)) {
+                notify(NOTIFICATION_ID, builder.build())
+            }
         }
     }
 
--- app/src/main/java/com/example/lifeprogress/ResultActivity.kt
+++ app/src/main/java/com/example/lifeprogress/ResultActivity.kt
@@ -1,7 +1,7 @@
 package com.example.lifeprogress
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
